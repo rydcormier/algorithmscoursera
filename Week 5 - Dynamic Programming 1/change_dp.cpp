1c105b58cbc00e28d8a4af921d1db111d97626f4
Ryan Cormier <rydcormier@gmail.com>
2018-03-03 16:19:53 -0500

    Almost done primitive_calculator.cpp

--- Week 5 - Dynamic Programming 1/change_dp.cpp
+++ Week 5 - Dynamic Programming 1/change_dp.cpp
@@ -1,8 +1,8 @@
 /**
  *  change_dp.cpp
  *
- *  This file implements a dynammic programming algorithm to solve the problem of 
- *  changing a monetary value.
+ *  This file implements a dynammic programming algorithm to sfind the minimum number
+ *  of coins needed to change a monetary value.
  *
  *  Author: Ryan Cormier <rydcormier@gmail.com>
  *  Date:   February 28, 2018
@@ -10,20 +10,22 @@
  **/
 
 #include <iostream>
-int NumCoins = 3;
-int Coins[] {1, 3, 4};
+
+struct coin_denominations {
+    int numCoins = 3;
+    int coins[] {1, 3, 4};
+} denominations;
 
 int get_change(int m) {
     
-    //First create an array
     int min_num_coins [m + 1];
     min_num_coins[0] = 0;
     
     for (int i = 1; i <= m; ++i) {
         min_num_coins[i] = 1001;
         
-        for (int j = 0; j < NumCoins; ++j) {
-            int coin = Coins[j];
+        for (int j = 0; j < denominations.numCoins; ++j) {
+            int coin = denominations.coins[j];
             
             if (i >= coin) {
                 int num_coins = min_num_coins[i - coin] + 1;
--- Week 5 - Dynamic Programming 1/primitive_calculator.cpp
+++ Week 5 - Dynamic Programming 1/primitive_calculator.cpp
@@ -4,7 +4,19 @@
 
 using std::vector;
 
-vector<int> optimal_sequence(int n) {
+vector<int> gen_basic_sequence(int n) {
+    vector<int> res(n);
+    for (int i = 1; i <= n; ++i) {
+        res[i - 1] = i;
+    }
+    return res;
+}
+
+int num_ops(vector<int> seq) {
+    return seq.size() - 1;
+}
+
+vector<int> optimal_sequence_greedy(int n) {
   std::vector<int> sequence;
   while (n >= 1) {
     sequence.push_back(n);
@@ -20,12 +32,61 @@ vector<int> optimal_sequence(int n) {
   return sequence;
 }
 
+vector<int> optimal_sequence(int num) {
+    vector<int> min_num_seq[num + 1];
+    min_num_seq[1] = vector<int>(1, 1);
+    
+    for (int n = 2; n <= num; ++n) {
+        min_num_seq[n] = gen_basic_sequence(n);
+        min_num_seq[0] = vector<int>();
+        min_num_seq[1] = vector<int>(1,1);
+        vector<int> seq;
+        
+        for (int n = 2; n <= num; ++n) {
+            // define longest sequence for n
+            min_num_seq[n] = gen_basic_sequence(n);
+            
+            //+1 op
+            seq = min_num_seq[n - 1];
+            seq.push_back(n);
+            if (num_ops(seq) < num_ops(min_num_seq[n])) {
+                min_num_seq[n] = seq;
+            }
+            
+            //*2 op
+            if (n % 2 == 0) {
+                seq = min_num_seq[n%2];
+                seq.push_back(n);
+                if (num_ops(seq) < num_ops(min_num_seq[n])) {
+                    min_num_seq[n] = seq;
+                }
+            }
+            
+            //*3 op
+            if (n % 2 == 0) {
+                seq = min_num_seq[n%2];
+                seq.push_back(n);
+                if (num_ops(seq) < num_ops(min_num_seq[n])) {
+                    min_num_seq[n] = seq;
+                }
+            }
+        }
+    }
+    
+    return min_num_seq[num];
+}
+
 int main() {
-  int n;
-  std::cin >> n;
-  vector<int> sequence = optimal_sequence(n);
-  std::cout << sequence.size() - 1 << std::endl;
-  for (size_t i = 0; i < sequence.size(); ++i) {
-    std::cout << sequence[i] << " ";
-  }
+    int n;
+    std::cin >> n;
+    vector<int> sequence = optimal_sequence_greedy(n);
+    std::cout << sequence.size() - 1 << std::endl;
+    for (size_t i = 0; i < sequence.size(); ++i) {
+        std::cout << sequence[i] << " ";
+    }
+    vector<int> sequence1 = optimal_sequence(n);
+    std::cout << sequence1.size() - 1 << std::endl;
+    for (size_t i = 0; i < sequence1.size(); ++i) {
+        std::cout << sequence1[i] << " ";
+    }
 }
