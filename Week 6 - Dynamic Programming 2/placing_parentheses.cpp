99b86a78b57868343621a66820710aea02dcbaf4
Ryan Cormier <rydcormier@gmail.com>
2018-03-16 16:17:08 -0400

    placing_parentheses.cpp is ready to submit

--- Week 6 - Dynamic Programming 2/placing_parentheses.cpp
+++ Week 6 - Dynamic Programming 2/placing_parentheses.cpp
@@ -1,5 +1,13 @@
 /**
  *  placing_parentheses.cpp
+ *
+ *  This file implements a dynamic programming algorithm to find the maximum
+ *  value of an arithmetic expression through manipulation of the order of
+ *  operations. Given the expression on the command line, the maximum value
+ *  is then printed to the console.
+ *
+ *  Author: Ryan Cormier <rydcormier@gmail.com>
+ *  Date:   March 16, 2018
  **/
 
 #include <iostream>
@@ -12,48 +20,15 @@ using std::string;
 using std::max;
 using std::min;
 
-/**
- *  struct Expression
- *
- *  Represents an arithmetic expression storing the digits and operations
- *  seperately.
- **/
-struct Expression {
-    vector<int> digits;
-    string ops;
+using Table = vector<vector<long long>>;
 
-    // Pad beginning to avoid offset indeces.
-    Expression() : digits(vector<int>(1, 0)), ops(" ") {}
-};
-
+// A pair of extreme values
 struct Range {
     long long minimum;
     long long maximum;
 
     Range() : minimum(0), maximum(0) {}
 };
-
-using Table = vector<vector<Range>>;
-
-/**
- *  function split_string
- *
- *  Takes an arithmetic expression string (i.e "1+2-3...") and returns an
- *  Expression object which stores the digits and operations of the expression.
- **/
-Expression split_string(const string &s) {
-    Expression exp;
-    assert(s.length() % 2 == 1); // must be odd number
-    for (size_t i = 0; i < s.length(); ++i) {
-        if (i % 2 == 0) {
-            int d = std::stoi(s.substr(i, 1));
-            exp.digits.push_back(d);
-        } else {
-            exp.ops.push_back(s[i]);
-        }
-    }
-    return exp;
-}
 
 long long eval(long long a, long long b, char op) {
     if (op == '*') {
@@ -67,58 +42,69 @@ long long eval(long long a, long long b, char op) {
     }
 }
 
-/**
- *  function min_and_max
- *
- *  Finds the minimum and maximum values of an expression. If indeces i and j
- *  are given, the subexpression from the ith to the jth digit is evaluated,
- *  else the entire expression is used.
- *
- *  Arguments:
- *      exp:    Expression to be evaluated
- *      table:  The table of minimum and maximum values already computed for
- *              previous subexpressions.
- *      i:      The starting index, or 1 if not given.
- *      j:      The ending index, or the end of the expression if not given.
- **/
-Range min_and_max(const Expression &exp, const Table &table, size_t i = 1,
-    size_t j = 0) {
-    if (i == j) return table[i][j];
-    if (j == 0) j = exp.digits.size();
+// Find the extreme values of a subexpression
+Range min_and_max(const string &ops, size_t i, size_t j, const Table &m, const Table &M) {
+    long long min_val, max_val;
     Range res;
-    for (size_t k = i; i < j; ++i) {
-        Range lhs = table[i][k], rhs = table[k + 1][j];
-        int m1 = lhs.minimum, m2 = rhs.minimum;
-        int M1 = lhs.maximum, M2 = rhs.maximum;
-        char op = exp.ops[k];
+    for (size_t k = i; k < j; ++k) {
+        long long M1 = M[i][k], M2 = M[k + 1][j];
+        long long m1 = m[i][k], m2 = m[k + 1][j];
+        char op = ops[k];
 
-        int a = eval(M1, M2, op);
-        int b = eval(M1, m2, op);
-        int c = eval(m1, M2, op);
-        int d = eval(m1, m2, op);
+        // Use all combinations of extreme values with kth operation.
+        long long a = eval(M1, M2, op);
+        long long b = eval(M1, m2, op);
+        long long c = eval(m1, M2, op);
+        long long d = eval(m1, m2, op);
 
-        res.minimum = min({a, b, c, d});
-        res.maximum = max({a, b, c, d});
+        if (k == i) {   // min_val and max_val not initiated yet
+            min_val = min({a, b, c, d});
+            max_val = max({a, b, c, d});
+        } else {
+            min_val = min({min_val, a, b, c, d});
+            max_val = max({max_val, a, b, c, d});
+        }
     }
+    res.minimum = min_val;
+    res.maximum = max_val;
     return res;
 }
 
+// Find the maximum value of an expression
 long long get_maximum_value(const string &s) {
-    Expression exp = split_string(s);
-    int n = exp.digits.size();
-    Table table(n, vector<Range>(n, Range()));
-    for (int i = 1; i < exp.digits.size(); ++i) {
-        int d = exp.digits[i];
-        table[i][i].minimum = d;
-        table[i][i].maximum = d;
+    int n = (s.length() + 1) / 2;
+
+    // Initialize tables for min and max values.
+    Table m(n + 1, vector<long long>(n + 1, 0));
+    Table M(n + 1, vector<long long>(n + 1, 0));
+
+    // Store operations as a string
+    string ops(" ");
+
+    size_t k = 1; // seperate index to initialize diagonals of the tables.
+    for (size_t i = 0; i < s.length(); ++i) {
+        if (i % 2 == 0) {
+            int d = std::stoi(s.substr(i, 1));
+            m[k][k] = d;
+            M[k][k] = d;
+            ++k;
+        } else {
+            ops.push_back(s[i]);
+        }
     }
+
+    // Compute exteme values of subexpressions in order of increasing j - i.
     for (size_t s = 1; s < n; ++s) {
         for (size_t i = 1; i <= n - s; ++i) {
             size_t j = i + s;
-            table[i][j] = min_and_max(exp, table, i, j);
+            Range range = min_and_max(ops, i, j, m, M);
+            m[i][j] = range.minimum;
+            M[i][j] = range.maximum;
         }
     }
-    return table[1][n].maximum;
+
+    // Max value is in the top right corner of the table M
+    return M[1][n];
 }
 
 int main() {
