65414ef572dc72b7cee4e0c44dbe1f08d91dcca0
Ryan Cormier <rydcormier@gmail.com>
2018-03-26 16:14:36 -0400

    Started tree-height.cpp

--- data_structures/week1/Programming-Assignment-1/tree_height/tree-height.cpp
+++ data_structures/week1/Programming-Assignment-1/tree_height/tree-height.cpp
@@ -12,68 +12,105 @@ public:
     int key;
     Node *parent;
     std::vector<Node *> children;
-
+    
     Node() {
-      this->parent = NULL;
+        this->parent = NULL;
     }
-
+    
     void setParent(Node *theParent) {
-      parent = theParent;
-      parent->children.push_back(this);
+        parent = theParent;
+        parent->children.push_back(this);
     }
 };
 
+int height(Node *node) {
+    if (node->children.empty()) {
+        return 1;
+    }
+    int max_height = 0;
+    for (size_t i = 0; i < node->children.size(); ++i) {
+        int h = height(node->children[i]);
+        if (h > max_height) max_height = h;
+    }
+    return 1 + max_height;
+}
+
 
 int main_with_large_stack_space() {
-  std::ios_base::sync_with_stdio(0);
-  int n;
-  std::cin >> n;
-
-  std::vector<Node> nodes;
-  nodes.resize(n);
-  for (int child_index = 0; child_index < n; child_index++) {
-    int parent_index;
-    std::cin >> parent_index;
-    if (parent_index >= 0)
-      nodes[child_index].setParent(&nodes[parent_index]);
-    nodes[child_index].key = child_index;
-  }
-
-  // Replace this code with a faster implementation
-  int maxHeight = 0;
-  for (int leaf_index = 0; leaf_index < n; leaf_index++) {
-    int height = 0;
-    for (Node *v = &nodes[leaf_index]; v != NULL; v = v->parent)
-      height++;
-    maxHeight = std::max(maxHeight, height);
-  }
+    std::ios_base::sync_with_stdio(0);
+    int n;
+    std::cin >> n;
     
-  std::cout << maxHeight << std::endl;
-  return 0;
+    std::vector<Node> nodes;
+    nodes.resize(n);
+    for (int child_index = 0; child_index < n; child_index++) {
+        int parent_index;
+        std::cin >> parent_index;
+        if (parent_index >= 0)
+            nodes[child_index].setParent(&nodes[parent_index]);
+        nodes[child_index].key = child_index;
+    }
+    
+    // Replace this code with a faster implementation
+    int maxHeight = 0;
+    for (int leaf_index = 0; leaf_index < n; leaf_index++) {
+        int height = 0;
+        for (Node *v = &nodes[leaf_index]; v != NULL; v = v->parent)
+            height++;
+        maxHeight = std::max(maxHeight, height);
+    }
+    
+    std::cout << maxHeight << std::endl;
+    return 0;
+}
+
+int main_with_large_stack_space1() {
+    std::ios_base::sync_with_stdio(0);
+    int n;
+    std::cin >> n;
+    
+    std::vector<Node> nodes;
+    nodes.resize(n);
+    int root_index;
+    for (int child_index = 0; child_index < n; child_index++) {
+        int parent_index;
+        std::cin >> parent_index;
+        if (parent_index >= 0) {
+            nodes[child_index].setParent(&nodes[parent_index]);
+        } else {
+            root_index = child_index;
+        }
+        nodes[child_index].key = child_index;
+    }
+    
+    int maxHeight = height(&nodes[root_index]);
+    
+    std::cout << maxHeight << std::endl;
+    return 0;
 }
 
 int main (int argc, char **argv)
 {
 #if defined(__unix__) || defined(__APPLE__)
-  // Allow larger stack space
-  const rlim_t kStackSize = 16 * 1024 * 1024;   // min stack size = 16 MB
-  struct rlimit rl;
-  int result;
-
-  result = getrlimit(RLIMIT_STACK, &rl);
-  if (result == 0)
-  {
-      if (rl.rlim_cur < kStackSize)
-      {
-          rl.rlim_cur = kStackSize;
-          result = setrlimit(RLIMIT_STACK, &rl);
-          if (result != 0)
-          {
-              std::cerr << "setrlimit returned result = " << result << std::endl;
-          }
-      }
-  }
-
+    // Allow larger stack space
+    const rlim_t kStackSize = 16 * 1024 * 1024;   // min stack size = 16 MB
+    struct rlimit rl;
+    int result;
+    
+    result = getrlimit(RLIMIT_STACK, &rl);
+    if (result == 0)
+    {
+        if (rl.rlim_cur < kStackSize)
+        {
+            rl.rlim_cur = kStackSize;
+            result = setrlimit(RLIMIT_STACK, &rl);
+            if (result != 0)
+            {
+                std::cerr << "setrlimit returned result = " << result << std::endl;
+            }
+        }
+    }
+    
 #endif
-  return main_with_large_stack_space();
+    return main_with_large_stack_space();
 }
