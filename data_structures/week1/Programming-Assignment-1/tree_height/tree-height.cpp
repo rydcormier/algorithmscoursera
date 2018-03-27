c3263c9073af4acc50879dd90809516d4d3f4266
Ryan Cormier <rydcormier@gmail.com>
2018-03-27 11:27:53 -0400

    Finished tree-height.cpp

--- data_structures/week1/Programming-Assignment-1/tree_height/tree-height.cpp
+++ data_structures/week1/Programming-Assignment-1/tree_height/tree-height.cpp
@@ -1,6 +1,7 @@
 #include <algorithm>
 #include <iostream>
 #include <vector>
+#include <queue>
 #if defined(__unix__) || defined(__APPLE__)
 #include <sys/resource.h>
 #endif
@@ -23,16 +24,31 @@ public:
     }
 };
 
-int height(Node *node) {
-    if (node->children.empty()) {
-        return 1;
+int treeHeight(Node *root) {
+    if (root == NULL) return 0;
+    int height = 0;
+    // use a queue for level traversal
+    std::queue<Node *> q;
+    q.push(root);
+    
+    while (true) {
+        // count the nodes on this level
+        int count = q.size();
+        // no nodes means we're done;
+        if (count == 0) return height;
+        
+        ++height;
+        
+        // Dequeue nodes and enque children
+        while (count > 0) {
+            Node *node = q.front();
+            q.pop();
+            for (int i = 0; i < node->children.size(); ++i) {
+                q.push(node->children[i]);
+            }
+            --count;
+        }
     }
-    int max_height = 0;
-    for (size_t i = 0; i < node->children.size(); ++i) {
-        int h = height(node->children[i]);
-        if (h > max_height) max_height = h;
-    }
-    return 1 + max_height;
 }
 
 
@@ -43,51 +59,35 @@ int main_with_large_stack_space() {
     
     std::vector<Node> nodes;
     nodes.resize(n);
-    for (int child_index = 0; child_index < n; child_index++) {
-        int parent_index;
-        std::cin >> parent_index;
-        if (parent_index >= 0)
-            nodes[child_index].setParent(&nodes[parent_index]);
-        nodes[child_index].key = child_index;
-    }
     
-    // Replace this code with a faster implementation
-    int maxHeight = 0;
-    for (int leaf_index = 0; leaf_index < n; leaf_index++) {
-        int height = 0;
-        for (Node *v = &nodes[leaf_index]; v != NULL; v = v->parent)
-            height++;
-        maxHeight = std::max(maxHeight, height);
-    }
-    
-    std::cout << maxHeight << std::endl;
-    return 0;
-}
-
-int main_with_large_stack_space1() {
-    std::ios_base::sync_with_stdio(0);
-    int n;
-    std::cin >> n;
-    
-    std::vector<Node> nodes;
-    nodes.resize(n);
+    // keep the index for the root to use with treeHeight
     int root_index;
     for (int child_index = 0; child_index < n; child_index++) {
         int parent_index;
         std::cin >> parent_index;
         if (parent_index >= 0) {
             nodes[child_index].setParent(&nodes[parent_index]);
-        } else {
+        } else {    // parent index of -1 means root
             root_index = child_index;
         }
         nodes[child_index].key = child_index;
     }
     
-    int maxHeight = height(&nodes[root_index]);
+    // Replace this code with a faster implementation
+//    int maxHeight = 0;
+//    for (int leaf_index = 0; leaf_index < n; leaf_index++) {
+//        int height = 0;
+//        for (Node *v = &nodes[leaf_index]; v != NULL; v = v->parent)
+//            height++;
+//        maxHeight = std::max(maxHeight, height);
+//    }
+    
+    int maxHeight = treeHeight(&nodes[root_index]);
     
     std::cout << maxHeight << std::endl;
     return 0;
 }
+
 
 int main (int argc, char **argv)
 {
