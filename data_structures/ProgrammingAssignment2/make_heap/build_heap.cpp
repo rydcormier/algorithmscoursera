5374577238763ccfe51e03060c3a08e020131cbf
Ryan Cormier <rydcormier@gmail.com>
2018-05-02 13:27:01 -0400

    First crack at build_heap.cpp

--- data_structures/ProgrammingAssignment2/make_heap/build_heap.cpp
+++ data_structures/ProgrammingAssignment2/make_heap/build_heap.cpp
@@ -1,3 +1,11 @@
+/**
+ *  build_heap.cpp
+ *  Data Structures: Programming Assignment 2
+ *
+ *  Author: Ryan Cormier <rydcormier@gmail.com>
+ *  Date:   May 2, 2018
+ **/
+
 #include <iostream>
 #include <vector>
 #include <algorithm>
@@ -10,54 +18,85 @@ using std::pair;
 using std::make_pair;
 
 class HeapBuilder {
- private:
-  vector<int> data_;
-  vector< pair<int, int> > swaps_;
+private:
+    vector<int> data_;
+    vector< pair<int, int> > swaps_;
 
-  void WriteResponse() const {
-    cout << swaps_.size() << "\n";
-    for (int i = 0; i < swaps_.size(); ++i) {
-      cout << swaps_[i].first << " " << swaps_[i].second << "\n";
+    int left_child(int i) {
+        return 2 * i + 1;
     }
-  }
 
-  void ReadData() {
-    int n;
-    cin >> n;
-    data_.resize(n);
-    for(int i = 0; i < n; ++i)
-      cin >> data_[i];
-  }
+    int right_child(int i) {
+        return 2 * i + 2;
+    }
 
-  void GenerateSwaps() {
-    swaps_.clear();
-    // The following naive implementation just sorts 
-    // the given sequence using selection sort algorithm
-    // and saves the resulting sequence of swaps.
-    // This turns the given array into a heap, 
-    // but in the worst case gives a quadratic number of swaps.
-    //
-    // TODO: replace by a more efficient implementation
-//    for (int i = 0; i < data_.size(); ++i)
-//      for (int j = i + 1; j < data_.size(); ++j) {
-//        if (data_[i] > data_[j]) {
-//          swap(data_[i], data_[j]);
-//          swaps_.push_back(make_pair(i, j));
+    void sift_down(int i) {
+        // restore heap condition for subtree with root at i
+        int min_index = i;
+        int l = left_child(i);
+        if (l < data_.size() && data_[l] < data_[min_index]) {
+            min_index = l;
+        }
+        int r = right_child(i);
+        if (r < data_.size() && data_[r] < data_[min_index]) {
+            min_index = r;
+        }
+        if (i != min_index) {
+            swap(data_[i], data_[min_index]);
+            swaps_.push_back(make_pair(i, min_index));
+            sift_down(min_index);
+        }
+    }
+
+    void WriteResponse() const {
+        cout << swaps_.size() << "\n";
+        for (int i = 0; i < swaps_.size(); ++i) {
+            cout << swaps_[i].first << " " << swaps_[i].second << "\n";
+        }
+    }
+
+    void ReadData() {
+        int n;
+        cin >> n;
+        data_.resize(n);
+        for(int i = 0; i < n; ++i)
+            cin >> data_[i];
+    }
+
+    void GenerateSwaps() {
+        swaps_.clear();
+        // The following naive implementation just sorts
+        // the given sequence using selection sort algorithm
+        // and saves the resulting sequence of swaps.
+        // This turns the given array into a heap,
+        // but in the worst case gives a quadratic number of swaps.
+        //
+        // TODO: replace by a more efficient implementation
+//        for (int i = 0; i < data_.size(); ++i) {
+//            for (int j = i + 1; j < data_.size(); ++j) {
+//                if (data_[i] > data_[j]) {
+//                    swap(data_[i], data_[j]);
+//                    swaps_.push_back(make_pair(i, j));
+//                }
+//            }
 //        }
-//      }
-  }
+        int n ((int) data_.size());
+        for (int i = (n / 2); i >= 0; i--) {
+            sift_down(i);   // swaps are generated in sift_down;
+        }
+    }
 
- public:
-  void Solve() {
-    ReadData();
-    GenerateSwaps();
-    WriteResponse();
-  }
+public:
+    void Solve() {
+        ReadData();
+        GenerateSwaps();
+        WriteResponse();
+    }
 };
 
 int main() {
-  std::ios_base::sync_with_stdio(false);
-  HeapBuilder heap_builder;
-  heap_builder.Solve();
-  return 0;
+    std::ios_base::sync_with_stdio(false);
+    HeapBuilder heap_builder;
+    heap_builder.Solve();
+    return 0;
 }
