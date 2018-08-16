48c08c3755cba027cab91870db61d71536229d65
Ryan Cormier <rydcormier@gmail.com>
2018-08-16 13:06:50 -0400

    Finished Programming Assignment 4.

--- algorithms_on_graphs/Programming Assignment 4/negative_cycle.cpp
+++ algorithms_on_graphs/Programming Assignment 4/negative_cycle.cpp
@@ -1,23 +1,77 @@
+//
+//  negative_cycle.cpp
+//
+//  Algorithms on Graphs - Programming Assignment 4
+//
+//  Created by Ryan Cormier on 8/15/18.
+//
+
 #include <iostream>
 #include <vector>
+#include <queue>
+#include <limits>
 
 using std::vector;
+using std::queue;
 
 int negative_cycle(vector<vector<int> > &adj, vector<vector<int> > &cost) {
-  //write your code here
-  return 0;
+    // Run |V| iterations of Bellman-Ford algorithm on an unexplored vertex, and if a
+    // distance is updated on last iteration, a negative cycle exists.
+    vector<int> dist(adj.size(), std::numeric_limits<int>::max());
+    
+    for (int s = 0; s < adj.size(); s++) {
+        // check if we've been here
+        if (dist[s] < std::numeric_limits<int>::max()) {
+            continue;
+        }
+        
+        dist[s] = 0;
+        bool dist_updated = false;
+        
+        for (int i = 0; i < adj.size(); i++) {
+            dist_updated = false;
+            queue<int> q;
+            q.push(s);
+            vector<bool> visited(adj.size(), false);
+            visited[s] = true;
+            while (!q.empty()) {
+                int u = q.front();
+                q.pop();
+                for (int j = 0; j < adj[u].size(); j++) {
+                    int v = adj[u][j];
+                    int w = cost[u][j];
+                    if (!visited[v]) {
+                        q.push(v);
+                    }
+                    if (dist[v] > dist[u] + w) {
+                        dist[v] = dist[u] + w;
+                        dist_updated = true;
+                    }
+                    visited[v] = true;
+                }
+            }
+            if (!dist_updated) {  // no change means we can break early
+                break;
+            }
+        }
+        // if an edge was relaxed, we have a negative cycle
+        if (dist_updated) {
+            return 1;
+        }
+    }
+    return 0;
 }
 
 int main() {
-  int n, m;
-  std::cin >> n >> m;
-  vector<vector<int> > adj(n, vector<int>());
-  vector<vector<int> > cost(n, vector<int>());
-  for (int i = 0; i < m; i++) {
-    int x, y, w;
-    std::cin >> x >> y >> w;
-    adj[x - 1].push_back(y - 1);
-    cost[x - 1].push_back(w);
-  }
-  std::cout << negative_cycle(adj, cost);
+    int n, m;
+    std::cin >> n >> m;
+    vector<vector<int> > adj(n, vector<int>());
+    vector<vector<int> > cost(n, vector<int>());
+    for (int i = 0; i < m; i++) {
+        int x, y, w;
+        std::cin >> x >> y >> w;
+        adj[x - 1].push_back(y - 1);
+        cost[x - 1].push_back(w);
+    }
+    std::cout << negative_cycle(adj, cost);
 }
--- algorithms_on_graphs/Programming Assignment 4/shortest_paths.cpp
+++ algorithms_on_graphs/Programming Assignment 4/shortest_paths.cpp
@@ -1,41 +1,171 @@
+//
+//  shortest_paths.cpp
+//
+//  Algorithms on Graphs - Programming Assignment 4
+//
+//  Created by Ryan Cormier on 8/15/18.
+//
 #include <iostream>
 #include <limits>
 #include <vector>
 #include <queue>
+#include <functional>
 
 using std::vector;
 using std::queue;
 using std::pair;
 using std::priority_queue;
 
-void shortest_paths(vector<vector<int> > &adj, vector<vector<int> > &cost, int s, vector<long long> &distance, vector<int> &reachable, vector<int> &shortest) {
-  //write your code here
+typedef pair<long long, int> Vertex;
+
+void shortest_paths( vector<vector<int> > &adj,
+                    vector<vector<int> > &cost,
+                    int s,
+                    vector<long long> &distance,
+                    vector<int> &reachable,
+                    vector<int> &shortest,
+                    bool has_negative_weights) {
+    
+    distance[s] = 0;
+    reachable[s] = 1;
+    // if no negative weights, use dijkstra
+    if (!has_negative_weights) {
+        priority_queue<Vertex, vector<Vertex>, std::greater<Vertex> > pq;
+        pq.push(std::make_pair(0, s));
+        
+        vector<bool> processed(adj.size(), false);
+        
+        while (!pq.empty()) {
+            Vertex u = pq.top();
+            pq.pop();
+            
+            if (processed[u.second]) {
+                continue;
+            }
+            
+            processed[u.second] = true;
+            
+            for (int i = 0; i < adj[u.second].size(); i++) {
+                int v = adj[u.second][i];
+                int w = cost[u.second][i];
+                reachable[v] = 1;
+                if (distance[v] > distance[u.second] + w) {
+                    distance[v] = distance[u.second] + w;
+                    pq.push(std::make_pair(distance[v], v));
+                }
+            }
+        }
+        return;
+    }
+    
+    // Use bellman ford |V| - 1 times
+    bool dist_updated = false;
+    int counter = 1;
+    while (counter < adj.size()) {
+        
+        queue<int> q;
+        q.push(s);
+        vector<bool> visited(adj.size(), false);
+        visited[s] = true;
+        
+        while (!q.empty()) {
+            int u = q.front();
+            q.pop();
+            for (int i = 0; i < adj[u].size(); i++) {
+                int v = adj[u][i];
+                int w = cost[u][i];
+                reachable[v] = 1;
+                if (distance[v] > distance[u] + w) {
+                    distance[v] = distance[u] + w;
+                    dist_updated = true;
+                }
+                if (!visited[v]) {
+                    q.push(v);
+                    visited[v] = true;
+                }
+            }
+        }
+        
+        if (!dist_updated) {
+            break;
+        }
+        counter++;
+    }
+    
+    if (dist_updated) {
+        // find updated vertices
+        queue<int> q;
+        q.push(s);
+        vector<bool> visited(adj.size(), false);
+        visited[s] = true;
+        
+        while (!q.empty()) {
+            int u = q.front();
+            q.pop();
+            for (int i = 0; i < adj[u].size(); i++) {
+                int v = adj[u][i];
+                int w = cost[u][i];
+                if (distance[v] > distance[u] + w) {
+                    distance[v] = distance[u] + w;
+                    shortest[v] = 0;
+                }
+                if (!visited[v]) {
+                    q.push(v);
+                    visited[v] = true;
+                }
+            }
+        }
+    }
+    
+    // BFS on previously found vertices
+    vector<bool> visited(adj.size(), false);
+    for (int i = 0; i < adj.size(); i++) {
+        if (!shortest[i] && !visited[i]) {
+            queue<int> q;
+            q.push(i);
+            while (!q.empty()) {
+                int u = q.front();
+                q.pop();
+                for (vector<int>::iterator v = adj[u].begin(); v != adj[u].end(); v++) {
+                    shortest[*v] = 0;
+                    if (!visited[*v]) {
+                        q.push(*v);
+                        visited[*v] = true;
+                    }
+                }
+            }
+        }
+    }
 }
 
 int main() {
-  int n, m, s;
-  std::cin >> n >> m;
-  vector<vector<int> > adj(n, vector<int>());
-  vector<vector<int> > cost(n, vector<int>());
-  for (int i = 0; i < m; i++) {
-    int x, y, w;
-    std::cin >> x >> y >> w;
-    adj[x - 1].push_back(y - 1);
-    cost[x - 1].push_back(w);
-  }
-  std::cin >> s;
-  s--;
-  vector<long long> distance(n, std::numeric_limits<long long>::max());
-  vector<int> reachable(n, 0);
-  vector<int> shortest(n, 1);
-  shortest_paths(adj, cost, s, distance, reachable, shortest);
-  for (int i = 0; i < n; i++) {
-    if (!reachable[i]) {
-      std::cout << "*\n";
-    } else if (!shortest[i]) {
-      std::cout << "-\n";
-    } else {
-      std::cout << distance[i] << "\n";
+    int n, m, s;
+    std::cin >> n >> m;
+    vector<vector<int> > adj(n, vector<int>());
+    vector<vector<int> > cost(n, vector<int>());
+    bool has_negative_weights = false;
+    for (int i = 0; i < m; i++) {
+        int x, y, w;
+        std::cin >> x >> y >> w;
+        adj[x - 1].push_back(y - 1);
+        cost[x - 1].push_back(w);
+        if (w < 0 ) {
+            has_negative_weights = true;
+        }
     }
-  }
+    std::cin >> s;
+    s--;
+    vector<long long> distance(n, std::numeric_limits<long long>::max());
+    vector<int> reachable(n, 0);
+    vector<int> shortest(n, 1);
+    shortest_paths(adj, cost, s, distance, reachable, shortest, has_negative_weights);
+    for (int i = 0; i < n; i++) {
+        if (!reachable[i]) {
+            std::cout << "*\n";
+        } else if (!shortest[i]) {
+            std::cout << "-\n";
+        } else {
+            std::cout << distance[i] << "\n";
+        }
+    }
 }
