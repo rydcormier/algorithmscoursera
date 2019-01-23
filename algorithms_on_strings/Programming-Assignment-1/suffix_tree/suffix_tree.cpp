d8f57f8aed0021b939e3458f1910ae6b0fd7ee6e
Ryan Cormier <rydcormier@gmail.com>
2019-01-23 13:19:36 -0500

    Modified suffix_tree.cpp

--- algorithms_on_strings/Programming-Assignment-1/suffix_tree/suffix_tree.cpp
+++ algorithms_on_strings/Programming-Assignment-1/suffix_tree/suffix_tree.cpp
@@ -6,14 +6,12 @@
 //
 
 #include <iostream>
-#include <map>
 #include <string>
 #include <vector>
 
 using std::cin;
 using std::cout;
 using std::endl;
-using std::map;
 using std::string;
 using std::vector;
 
