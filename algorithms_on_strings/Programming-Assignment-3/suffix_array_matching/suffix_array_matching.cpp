6e0e04db39473734cd2415ebc379ed132bc28200
Ryan Cormier <rydcormier@gmail.com>
2019-05-23 14:56:56 -0400

    First attempt at suffix_array_matching.cpp

--- algorithms_on_strings/Programming-Assignment-3/suffix_array_matching/suffix_array_matching.cpp
+++ algorithms_on_strings/Programming-Assignment-3/suffix_array_matching/suffix_array_matching.cpp
@@ -10,6 +10,8 @@
 #include <string>
 #include <vector>
 
+using std::cout;
+using std::endl;
 using std::cin;
 using std::string;
 using std::vector;
@@ -23,19 +25,19 @@ size_t get_char_index(char c)
         case 'A':
             return 1;
             break;
-
+            
         case 'C':
             return 2;
             break;
-
+            
         case 'G':
             return 3;
             break;
-
+            
         case 'T':
             return 4;
             break;
-
+            
         default:
             return 0;
             break;
@@ -47,19 +49,19 @@ vector<size_t> sort_characters(const string& text)
 {
     vector<size_t> order(text.length());
     vector<size_t> count(NUM_CHARS);
-
+    
     for (size_t i = 0; i < text.length(); ++i)
     {
         count[get_char_index(text[i])] += 1;
     }
-
+    
     for (size_t j = 1; j < NUM_CHARS; ++j)
     {
         count[j] = count[j] + count[j - 1];
     }
-
+    
     size_t i = text.length();
-
+    
     while (i > 0)
     {
         --i;
@@ -67,7 +69,7 @@ vector<size_t> sort_characters(const string& text)
         count[c] -= 1;
         order[count[c]] = i;
     }
-
+    
     return order;
 }
 
@@ -78,17 +80,17 @@ vector<size_t> compute_character_classes(const string& text,
     vector<size_t> classes(text.length());
     classes[order[0]] = 0;
     size_t current_class = 0;
-
+    
     for (size_t i = 1; i < text.length(); ++i)
     {
         if (text[order[i]] != text[order[i - 1]])
         {
             ++current_class;
         }
-
+        
         classes[order[i]] =  current_class;
     }
-
+    
     return classes;
 }
 
@@ -101,19 +103,19 @@ vector<size_t> sort_doubled(const string& text,
     size_t s = text.length();
     vector<size_t> new_order(s);
     vector<size_t> count(s, 0);
-
+    
     for (size_t i = 0; i < s; ++i)
     {
         count[classes[i]] += 1;
     }
-
+    
     for (size_t j = 1; j < s; ++j)
     {
         count[j] += count[j - 1];
     }
-
+    
     size_t i = s;
-
+    
     while (i > 0)
     {
         --i;
@@ -122,7 +124,7 @@ vector<size_t> sort_doubled(const string& text,
         count[cl] -= 1;
         new_order[count[cl]] = start;
     }
-
+    
     return new_order;
 }
 
@@ -133,14 +135,14 @@ vector<size_t> update_classes(const vector<size_t>& order,
     size_t n = order.size();
     vector<size_t> new_classes(n);
     new_classes[order[0]] = 0;
-
+    
     for (size_t i = 1; i < n; ++i)
     {
         size_t cur = order[i];
         size_t prev = order[i - 1];
         size_t mid = (cur + length) % n;
         size_t mid_prev = (prev + length) % n;
-
+        
         if (classes[cur] != classes[prev] || classes[mid] != classes[mid_prev])
         {
             new_classes[cur] = new_classes[prev] + 1;
@@ -150,43 +152,44 @@ vector<size_t> update_classes(const vector<size_t>& order,
             new_classes[cur] = new_classes[prev];
         }
     }
-
+    
     return new_classes;
 }
 
-// Build suffix array of the string text and
-// return a vector result of the same length as the text
-// such that the value result[i] is the index (0-based)
-// in text where the i-th lexicographically smallest
-// suffix of text starts.
+/*  Build suffix array of the string text and return a vector result of the same length
+ *  as the text such that the value result[i] is the index (0-based) in text where the
+ *  i-th lexicographically smallest suffix of text starts.
+ */
 vector<size_t> BuildSuffixArray(const string& text)
 {
     vector<size_t> order = sort_characters(text);
     vector<size_t> classes = compute_character_classes(text, order);
     size_t l = 1;
-
+    
     while (l < text.length())
     {
         order = sort_doubled(text, l, order, classes);
         classes = update_classes(order, classes, l);
         l = 2 * l;
     }
-
+    
     return order;
 }
 
+// Return all the positions in text where a given pattern occurs.
 vector<size_t> FindOccurrences(const string& pattern, const string& text,
                                const vector<size_t>& suffix_array)
 {
     vector<size_t> result;
     size_t min_index = 0;
     size_t max_index = text.length();
-
+    
     while (min_index < max_index)
     {
         size_t mid_index = (min_index + max_index) / 2;
-
-        if (pattern > text.substr(suffix_array[mid_index]))
+        string suffix = text.substr(suffix_array[mid_index], pattern.length());
+        
+        if (pattern > suffix)
         {
             min_index = mid_index + 1;
         }
@@ -195,15 +198,16 @@ vector<size_t> FindOccurrences(const string& pattern, const string& text,
             max_index = mid_index;
         }
     }
-
+    
     size_t start = min_index;
     max_index = text.length();
-
+    
     while (min_index < max_index)
     {
         size_t mid_index = (min_index + max_index) / 2;
+        string suffix = text.substr(suffix_array[mid_index], pattern.length());
 
-        if (pattern < text.substr(suffix_array[mid_index]))
+        if (pattern < suffix)
         {
             max_index = mid_index;
         }
@@ -212,14 +216,14 @@ vector<size_t> FindOccurrences(const string& pattern, const string& text,
             min_index = mid_index + 1;
         }
     }
-
+    
     size_t end = max_index;
-
-    for (size_t i = start; i <= end; ++i)
+    
+    for (size_t i = start; i < end; ++i)
     {
-        result.push_back(i);
+        result.push_back(suffix_array[i]);
     }
-
+    
     return result;
 }
 
@@ -227,35 +231,36 @@ int main()
 {
     char buffer[100001];
     scanf("%s", buffer);
-
+    
     string text = buffer;
     text += '$';
     vector<size_t> suffix_array = BuildSuffixArray(text);
+    
     int pattern_count;
-
+    
     scanf("%d", &pattern_count);
-
+    
     vector<bool> occurs(text.length(), false);
-
+    
     for (int pattern_index = 0; pattern_index < pattern_count; ++pattern_index)
     {
         scanf("%s", buffer);
         string pattern = buffer;
         vector<size_t> occurrences = FindOccurrences(pattern, text, suffix_array);
-
+        
         for (int j = 0; j < occurrences.size(); ++j)
         {
             occurs[occurrences[j]] = true;
         }
     }
-
+    
     for (int i = 0; i < occurs.size(); ++i)
     {
         if (occurs[i]) {
             printf("%d ", i);
         }
     }
-
+    
     printf("\n");
     return 0;
 }
