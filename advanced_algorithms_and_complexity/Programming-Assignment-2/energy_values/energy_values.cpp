17ff51a0ac4f4ef5151693c7b2b0eb12e254f6c3
Ryan Cormier <rydcormier@gmail.com>
2019-06-14 14:44:49 -0400

    Started testing energy_values.cpp.

--- advanced_algorithms_and_complexity/Programming-Assignment-2/energy_values/energy_values.cpp
+++ advanced_algorithms_and_complexity/Programming-Assignment-2/energy_values/energy_values.cpp
@@ -1,3 +1,9 @@
+// energy_values.cpp
+//
+// Advanced Algorithms and Complexity - Programming Assignment 2
+// Created by Ryan Cormier <rydcormier@gmail.com> on June 14, 2019.
+//
+
 #include <cmath>
 #include <iostream>
 #include <vector>
@@ -9,96 +15,155 @@ typedef std::vector<double> Column;
 typedef std::vector<double> Row;
 typedef std::vector<Row> Matrix;
 
-struct Equation {
-    Equation(const Matrix &a, const Column &b):
-        a(a),
-        b(b)
-    {}
-
-    Matrix a;
-    Column b;
-};
-
-struct Position {
-    Position(int column, int row):
-        column(column),
-        row(row)
-    {}
-
+struct Position
+{
+    Position(int column, int row): column(column), row(row) {}
+    
     int column;
     int row;
+    
+    bool is_good(const Matrix& a)
+    {
+        // checks if row and column are valid indeces for a given matrix
+        if (row < 0 || column < 0 || a.empty())
+            return false;
+        if (row >= a.size() || column >= a[0].size())
+            return false;
+        return true;
+    }
 };
 
-Equation ReadEquation() {
+Matrix ReadInput()
+{
     int size;
     std::cin >> size;
-    Matrix a(size, std::vector <double> (size, 0.0));
-    Column b(size, 0.0);
-    for (int row = 0; row < size; ++row) {
-        for (int column = 0; column < size; ++column)
-            std::cin >> a[row][column];
-        std::cin >> b[row];
+    Matrix a(size, Row(size + 1, 0.0));
+    
+    for (int r = 0; r < size; ++r)
+    {
+        for (int c = 0; c <= size; ++c)
+        {
+            std::cin >> a[r][c];
+        }
     }
-    return Equation(a, b);
+    
+    return a;
 }
 
-Position SelectPivotElement(
-  const Matrix &a, 
-  std::vector <bool> &used_rows, 
-  std::vector <bool> &used_columns) {
-    // This algorithm selects the first free element.
-    // You'll need to improve it to pass the problem.
-    Position pivot_element(0, 0);
-    while (used_rows[pivot_element.row])
-        ++pivot_element.row;
-    while (used_columns[pivot_element.column])
-        ++pivot_element.column;
-    return pivot_element;
-}
-
-void SwapLines(Matrix &a, Column &b, std::vector <bool> &used_rows, Position &pivot_element) {
-    std::swap(a[pivot_element.column], a[pivot_element.row]);
-    std::swap(b[pivot_element.column], b[pivot_element.row]);
-    std::swap(used_rows[pivot_element.column], used_rows[pivot_element.row]);
-    pivot_element.row = pivot_element.column;
-}
-
-void ProcessPivotElement(Matrix &a, Column &b, const Position &pivot_element) {
-    // Write your code here
-}
-
-void MarkPivotElementUsed(const Position &pivot_element, std::vector <bool> &used_rows, std::vector <bool> &used_columns) {
-    used_rows[pivot_element.row] = true;
-    used_columns[pivot_element.column] = true;
-}
-
-Column SolveEquation(Equation equation) {
-    Matrix &a = equation.a;
-    Column &b = equation.b;
-    int size = a.size();
-
-    std::vector <bool> used_columns(size, false);
-    std::vector <bool> used_rows(size, false);
-    for (int step = 0; step < size; ++step) {
-        Position pivot_element = SelectPivotElement(a, used_rows, used_columns);
-        SwapLines(a, b, used_rows, pivot_element);
-        ProcessPivotElement(a, b, pivot_element);
-        MarkPivotElementUsed(pivot_element, used_rows, used_columns);
+void RescaleRow(Matrix& a, Position& p)
+{
+    // Multiply a row of a matrix given by position such that the value
+    // at position becomes 1.
+    double val = a[p.row][p.column];
+    
+    if (val == 1 || val == 0)
+        return;
+    for (int i = 0; i <= a.size(); ++i)
+    {
+        a[p.row][i] *= (1 / val);
     }
-
-    return b;
 }
 
-void PrintColumn(const Column &column) {
-    int size = column.size();
+void SubtractRows(Matrix& a, int row, Position& p)
+{
+    // Element by element subtraction from the given row index by the row
+    // given by position. Only the first row is changed so that the value
+    // of the row at the positions column becomes zero.
+    if (a[p.row][p.column] != 1)
+        RescaleRow(a, p);
+    double s = a[row][p.column];
+    
+    for (int i = 0; i <= a.size(); ++i)
+    {
+        a[row][i] = a[row][i] - (s * a[p.row][i]);
+    }
+}
+
+Position FindNextPivot(const Matrix& a, const Position& last_pivot)
+{
+    // Find the next farthest left leading entry from the last pivot position.
+    int n = (int) a.size();
+    int r = last_pivot.row + 1;
+    int c = last_pivot.column + 1;
+    
+    while (Position(r, c).is_good(a) && a[r][c] == 0)
+    {
+        if ( r < n - 1 )
+        {
+            r++;
+        }
+        else
+        {
+            r = last_pivot.row + 1;
+            c++;
+        }
+    }
+    return Position(r, c);
+}
+
+void SwapRows(Matrix& a, int i, int j)
+{
+    // Move the row at index j to the ith position and the row at i to the
+    // jth position.
+    Row temp(a[i]);
+    a[i] = a[j];
+    a[j] = temp;
+}
+
+Column SolveSystem(Matrix &a)
+{
+    // Gaussion row reduction on a matrix to reduced echelon form.  Returns the
+    // last column of the resulting matrix.
+    Column result(a.size());
+    
+    Position pivot = FindNextPivot(a, Position(-1, -1));
+    int r = 0;
+    while (pivot.is_good(a))
+    {
+        SwapRows(a, r, pivot.row);
+        pivot.row = r;
+        
+        for (int i = 0; i < a.size(); ++i)
+        {
+            if (i == pivot.row || a[i][pivot.column] == 0)
+                continue;
+            
+            SubtractRows(a, i, pivot);
+        }
+        
+        r++;
+        pivot = FindNextPivot(a, pivot);
+    }
+    
+    for (int i = 0; i < a.size(); ++i)
+    {
+        result[i] = a[i].back();
+    }
+    
+    return result;
+}
+
+void PrintColumn(const Column &column)
+{
+    int size = (int) column.size();
     std::cout.precision(PRECISION);
     for (int row = 0; row < size; ++row)
         std::cout << column[row] << std::endl;
 }
 
-int main() {
-    Equation equation = ReadEquation();
-    Column solution = SolveEquation(equation);
+Matrix test_matrix()
+{
+    Matrix a(2, Row(3, 0.0));
+    a[0][0] = 5; a[0][1] = -5; a[0][2] = -1;
+    a[1][0] = -1; a[1][1] = -2; a[1][2] = -1;
+    
+    return a;
+}
+
+int main()
+{
+    Matrix a = test_matrix(); // ReadInput();
+    Column solution = SolveSystem(a);
     PrintColumn(solution);
     return 0;
 }
