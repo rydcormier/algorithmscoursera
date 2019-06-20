d2791a793c2aef639f8297a42e176c8e11ab3d67
Ryan Cormier <rydcormier@gmail.com>
2019-06-20 12:30:40 -0400

    Edited energy_values.cpp.

--- advanced_algorithms_and_complexity/Programming-Assignment-2/energy_values/energy_values.cpp
+++ advanced_algorithms_and_complexity/Programming-Assignment-2/energy_values/energy_values.cpp
@@ -4,6 +4,7 @@
 // Created by Ryan Cormier <rydcormier@gmail.com> on June 14, 2019.
 //
 
+
 #include <cmath>
 #include <iostream>
 #include <vector>
@@ -15,155 +16,123 @@ typedef std::vector<double> Column;
 typedef std::vector<double> Row;
 typedef std::vector<Row> Matrix;
 
-struct Position
+void PrintSolution(const Matrix& a)
 {
-    Position(int column, int row): column(column), row(row) {}
+    size_t n = a.size();
+    std::cout.precision(PRECISION);
     
-    int column;
-    int row;
-    
-    bool is_good(const Matrix& a)
+    for (size_t i = 0; i  < n; ++i)
     {
-        // checks if row and column are valid indeces for a given matrix
-        if (row < 0 || column < 0 || a.empty())
-            return false;
-        if (row >= a.size() || column >= a[0].size())
-            return false;
-        return true;
+        std::cout << a[i][n] << std::endl;
     }
-};
+}
 
 Matrix ReadInput()
 {
     int size;
     std::cin >> size;
-    Matrix a(size, Row(size + 1, 0.0));
+    Matrix a(size, std::vector <double> (size + 1, 0.0));
     
-    for (int r = 0; r < size; ++r)
-    {
-        for (int c = 0; c <= size; ++c)
-        {
-            std::cin >> a[r][c];
-        }
-    }
-    
-    return a;
-}
-
-void RescaleRow(Matrix& a, Position& p)
-{
-    // Multiply a row of a matrix given by position such that the value
-    // at position becomes 1.
-    double val = a[p.row][p.column];
-    
-    if (val == 1 || val == 0)
-        return;
-    for (int i = 0; i <= a.size(); ++i)
-    {
-        a[p.row][i] *= (1 / val);
-    }
-}
-
-void SubtractRows(Matrix& a, int row, Position& p)
-{
-    // Element by element subtraction from the given row index by the row
-    // given by position. Only the first row is changed so that the value
-    // of the row at the positions column becomes zero.
-    if (a[p.row][p.column] != 1)
-        RescaleRow(a, p);
-    double s = a[row][p.column];
-    
-    for (int i = 0; i <= a.size(); ++i)
-    {
-        a[row][i] = a[row][i] - (s * a[p.row][i]);
-    }
-}
-
-Position FindNextPivot(const Matrix& a, const Position& last_pivot)
-{
-    // Find the next farthest left leading entry from the last pivot position.
-    int n = (int) a.size();
-    int r = last_pivot.row + 1;
-    int c = last_pivot.column + 1;
-    
-    while (Position(r, c).is_good(a) && a[r][c] == 0)
-    {
-        if ( r < n - 1 )
-        {
-            r++;
-        }
-        else
-        {
-            r = last_pivot.row + 1;
-            c++;
-        }
-    }
-    return Position(r, c);
-}
-
-void SwapRows(Matrix& a, int i, int j)
-{
-    // Move the row at index j to the ith position and the row at i to the
-    // jth position.
-    Row temp(a[i]);
-    a[i] = a[j];
-    a[j] = temp;
-}
-
-Column SolveSystem(Matrix &a)
-{
-    // Gaussion row reduction on a matrix to reduced echelon form.  Returns the
-    // last column of the resulting matrix.
-    Column result(a.size());
-    
-    Position pivot = FindNextPivot(a, Position(-1, -1));
-    int r = 0;
-    while (pivot.is_good(a))
-    {
-        SwapRows(a, r, pivot.row);
-        pivot.row = r;
-        
-        for (int i = 0; i < a.size(); ++i)
-        {
-            if (i == pivot.row || a[i][pivot.column] == 0)
-                continue;
-            
-            SubtractRows(a, i, pivot);
-        }
-        
-        r++;
-        pivot = FindNextPivot(a, pivot);
-    }
-    
-    for (int i = 0; i < a.size(); ++i)
-    {
-        result[i] = a[i].back();
-    }
-    
-    return result;
-}
-
-void PrintColumn(const Column &column)
-{
-    int size = (int) column.size();
-    std::cout.precision(PRECISION);
     for (int row = 0; row < size; ++row)
-        std::cout << column[row] << std::endl;
-}
-
-Matrix test_matrix()
-{
-    Matrix a(2, Row(3, 0.0));
-    a[0][0] = 5; a[0][1] = -5; a[0][2] = -1;
-    a[1][0] = -1; a[1][1] = -2; a[1][2] = -1;
+    {
+        for (int column = 0; column <= size; ++column)
+            std::cin >> a[row][column];
+    }
     
     return a;
+}
+
+int ForwardReduction(Matrix& a)
+{
+    size_t n = a.size();
+    for (size_t col = 0; col < n; ++col)
+    {
+        // Find the element in column with largest amplitude
+        size_t r_max = col;
+        double v_max = a[r_max][col];
+        
+        for (size_t row = col + 1; row < n; ++row)
+        {
+            if (std::fabs(a[row][col]) > v_max)
+            {
+                v_max = a[row][col];
+                r_max = row;
+            }
+        }
+        
+        // If the element is not on the diagonal, swap rows.
+        if (r_max != col)
+            std::swap(a[col], a[r_max]);
+        
+        size_t row = col;
+        
+        // Rescale row
+        if (a[row][col] != 1)
+        {
+            double val = a[row][col];
+            a[row][col] = 1;
+            
+            for (size_t c = col + 1; c <= n; ++c)
+            {
+                a[row][c] *= (1 / val);
+            }
+        }
+        
+        // fill the rest of the column with zeros.
+        for (size_t r = row + 1; r < n; ++r)
+        {
+            if (a[r][col] != 0)
+            {
+                double scalar = a[r][col];
+                a[r][col] = 0;
+                
+                for (size_t c = col + 1; c <= n; ++c)
+                {
+                    a[r][c] -= (scalar * a[row][c]);
+                }
+            }
+        }
+    }
+    
+    // This is only for systems with a single solution.
+    return 0;
+}
+
+void BackwardReduction(Matrix& a)
+{
+    int n = (int) a.size();
+    
+    // Start from bottom right fill the rest of the column with zeros
+    for (int col = n - 1; col > 0; --col)
+    {
+        for (int row = col - 1; row >= 0; --row)
+        {
+            if (a[row][col] != 0)
+            {
+                double scalar = a[row][col];
+                a[row][col] = 0;
+                a[row][n] -= (scalar * a[col][n]);
+            }
+        }
+    }
+}
+
+void Solve(Matrix& a)
+{
+    // reduce to REF
+    int flag = ForwardReduction(a);
+    
+    if (flag != 0)
+        return;
+    
+    // reduce to RREF
+    BackwardReduction(a);
 }
 
 int main()
 {
-    Matrix a = test_matrix(); // ReadInput();
-    Column solution = SolveSystem(a);
-    PrintColumn(solution);
+    Matrix a = ReadInput();
+    Solve(a);
+    PrintSolution(a);
     return 0;
 }
