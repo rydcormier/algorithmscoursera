ebea72bf22b0f10e5c55be44568032a5231a3a5f
Ryan Cormier <rydcormier@gmail.com>
2020-06-18 10:18:53 -0400

    Started phi X174 genome assembly with error prone reads and using an overlap graph.

---
+++ Genome Assembly Programming Challenge/Programming Assignment 1/phiX174_error_prone_overlap.cpp
@@ -0,0 +1,24 @@
+//
+//  phiX174_error_prone_overlap.cpp
+//
+//  Created by Ryan Cormier on 06/18/20
+//
+//  This programs reads 1618 reads of length 100 from standard input and
+//  then assembles and returns the circular genome from which the reads
+//  came. This program uses an overlap graph that accounts for possible
+//  errors in the reads when calculating edge weights. A Hamiltonian
+//  path is constructed maximizing overlap.
+//
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+// TODO: everything
+
+int main() 
+{
+    return 0;
+}
