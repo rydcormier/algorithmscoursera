de17d7a0f10053f4bd9af44defe2c8360aa05c12
Ryan Cormier <rydcormier@gmail.com>
2020-07-10 12:24:37 -0400

    Setting up program structure.

---
+++ GenomeAssembler/src/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  GenomeAssembler
+//
+//  Author: Ryan Cormier <rydcormier@gmail.com>
+//  Date:   July 10, 2020
+//
+
+#include <iostream>
+
+int main()
+{
+    // TODO: everything
+    return 0;
+}
